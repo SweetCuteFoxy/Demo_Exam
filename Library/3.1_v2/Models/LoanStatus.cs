8ba6310cced1deb32f888024711e5dc33748d433
SdfSWEET <godlikehokage85@gmail.com>
2026-03-25 16:20:00 +0500

    Add Library/3.1_v2 (polish, fix warnings)

---
+++ Library/3.1_v2/Models/LoanStatus.cs
@@ -0,0 +1,9 @@
+namespace LibraryV2.Models
+{
+    public class LoanStatus
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<BookLoan> BookLoans { get; set; } = new List<BookLoan>();
+    }
+}
---
+++ Library/3.1_v2/Models/Publisher.cs
@@ -0,0 +1,9 @@
+namespace LibraryV2.Models
+{
+    public class Publisher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public ICollection<Book> Books { get; set; } = new List<Book>();
+    }
+}
